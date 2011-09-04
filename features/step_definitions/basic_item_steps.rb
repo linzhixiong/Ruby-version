cfa6f5449d210ff37533a6a97b0fc812a9c1297c
林志雄 <darlinglele@live.com>
2011-09-04 20:50:21 +0800

    df

--- features/step_definitions/basic_item_steps.rb
+++ features/step_definitions/basic_item_steps.rb
@@ -1,4 +1,3 @@
-
 Given /^have a basic item with original price (.*)/ do |price|
   @basic_item=BasicItem.new(false, false, price)
 end
@@ -11,6 +10,6 @@ end
 When /^I view its final price$/ do
   @final_price=@basic_item.get_final_price
 end
-Then /^I should see final price is (.*)$/ do|final_price|
-@final_price.should==final_price.to_f
+Then /^I should see final price is (.*)$/ do |final_price|
+  @final_price.should==final_price.to_f
 end
